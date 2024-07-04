3b4dab2a3428f7f19c962c678fe23f39b5680ddb
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-03 21:28:24 -0400

    In this example we are going to create a counter that uses the useState based on the previous State.

---
+++ state-hook/src/components/HookCounterTwo.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+
+function HookCounterTwo() {
+  const initialCount = 0
+  const [count, setCount] = useState(initialCount)
+  
+    return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HookCounterTwo
