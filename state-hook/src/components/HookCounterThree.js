cccf41d9b4318c291911df4d2201e09021d35c76
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-04 20:46:11 -0400

    using an object as a state variable with the useState hook

---
+++ state-hook/src/components/HookCounterThree.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+function HookCounterThree() {
+    const [name, setName] = useState({firstName: '', lastName: ''})
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HookCounterThree
