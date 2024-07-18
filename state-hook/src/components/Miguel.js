aa5996becc773fc60f9f4b539f9c7478af57aa7a
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-18 17:20:07 -0400

    Practica simple creando un counter con useState y useEffect.

--- state-hook/src/components/Miguel.js
+++ state-hook/src/components/Miguel.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
-export default function Miguel() {
-  return <div>Miguel</div>;
+export default function MiguelCounter() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("state has changed");
+  }, [counter])
+
+  return (
+    <div>
+      <h1>Miguel Counter</h1>
+      <button onClick={() => setCounter(counter + 1)}>Add 1</button>
+      <p>The number goes up by 1 with every click: {counter}</p>
+    </div>
+  );
 }
