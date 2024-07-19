ccd2f45a66e9e91cad7325896e478aed40cd324c
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-18 21:20:15 -0400

    In this example we used the HookMouse component to mimic the ClassMouse component effect of the component did mount. Pay close attention to the useEffect in the HookMouse because the second parameter tells that thsi use effect will only run once, if the empty brackets are not added then the useEffect will run every time the event listener is activated with the mouse move, but adding the empty [ ] will only focus on the event listener and not in the useEffect

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -8,18 +8,22 @@ import HookCounterThree from "./components/HookCounterThree";
 import HookCounterTwo from "./components/HookCounterTwo";
 import MiguelCounter from "./components/Miguel";
 import { useState } from "react";
+import ClassMouse from "./components/ClassMouse";
+import HookMouse from "./components/HookMouse";
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
       {/* <HookCounterTwo /> */}
       {/* <HookCounterThree /> */}
       {/* <HookCounterFour /> */}
       {/* <MiguelCounter /> */}
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }
